fc681537e11e4ed5a4b9e8d44236dd61aa7eb6c6
heguodong <heguodong.uh@haier.com>
2019-01-10 17:26:45 +0800

    add unzip function on 2019 0110 1727.

--- app/src/main/java/com/haier/demo/testflippablestackview/MainActivity.java
+++ app/src/main/java/com/haier/demo/testflippablestackview/MainActivity.java
@@ -3,10 +3,16 @@ package com.haier.demo.testflippablestackview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 
 import com.haier.demo.testflippablestackview.helper.viewpagerhelper.FlippableStackView;
 import com.haier.demo.testflippablestackview.helper.viewpagerhelper.StackPageTransformer;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,12 +29,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mFlippableStack = findViewById(R.id.flippable_stack_view);
+
+/*
+        //把资产目录下文件推到sd卡中,
+        Util.copyDataFromAssetsToSDcard(this,"/mnt/sdcard/smartspeaker","assets.zip");
+
+        //解压数据并删除zip包
+        try {
+            File file = new File("/mnt/sdcard/smartspeaker/assets.zip");
+            Util.upZipFile(file,"/mnt/sdcard/smartspeaker/banner/");//后期需要遍历这个文件夹
+            boolean isDeleteSuccess = file.delete();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e("heguodong","Exception ->" + e.getLocalizedMessage());
+        }
+
+        //TODO:初始化数据，并返回两个list
+
+
+*/
+
+
+
+        //初始化数据
         createViewPagerFragments();
 
         mPageAdapter = new CardFragmentAdapter(getSupportFragmentManager(), mViewPagerFragments);
         mFlippableStack.initStack(3,StackPageTransformer.Orientation.VERTICAL );
         mFlippableStack.setAdapter(mPageAdapter);
+
+
+
+        //点击按钮的时候，去加载sd卡中的资源以及文件
+
+        //点击图片的时候，加载对应的资源
     }
+
+
 
     private void createViewPagerFragments() {
         mViewPagerFragments = new ArrayList<>();
---
+++ app/src/main/java/com/haier/demo/testflippablestackview/Util.java
@@ -0,0 +1,110 @@
+package com.haier.demo.testflippablestackview;
+
+import android.content.Context;
+import android.util.Log;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipException;
+import java.util.zip.ZipFile;
+
+/**
+ * Created by 01438511 on 2019/1/10.
+ */
+
+public class Util {
+
+    public static void copyDataFromAssetsToSDcard(Context context,String pathLocal,String filename) {
+        try {
+            InputStream myInput;
+            File file = new File(pathLocal);
+            if (!file.exists()){
+                file.mkdir();
+            }
+            File targetFile = new File(pathLocal + "/" + filename);
+            OutputStream myOutput = new FileOutputStream(targetFile);
+            myInput = context.getAssets().open("assets.zip");
+            byte[] buffer = new byte[1024];
+            int length = myInput.read(buffer);
+            while(length > 0)
+            {
+                myOutput.write(buffer, 0, length);
+                length = myInput.read(buffer);
+            }
+
+            myOutput.flush();
+            myInput.close();
+            myOutput.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e("heguodong","Exception ->" + e.getLocalizedMessage());
+        }
+    }
+
+     /**
+      * 解压缩功能.
+      * 将zipFile文件解压到folderPath目录下.
+      *
+      * @throws Exception
+      */
+     public static int upZipFile(File zipFile, String folderPath)throws ZipException,IOException {
+
+         ZipFile zfile=new ZipFile(zipFile);
+         Enumeration zList=zfile.entries();
+         ZipEntry ze=null;
+         byte[] buf=new byte[1024];
+         while(zList.hasMoreElements()){
+             ze=(ZipEntry)zList.nextElement();
+             if(ze.isDirectory()){
+                 Log.d("upZipFile", "ze.getName() = "+ze.getName());
+                 String dirstr = folderPath + ze.getName();
+                 //dirstr.trim();
+                 dirstr = new String(dirstr.getBytes("8859_1"), "GB2312");
+                 Log.d("upZipFile", "str = "+dirstr);
+                 File f=new File(dirstr);
+                 f.mkdir();
+                 continue;
+             }
+             Log.d("upZipFile", "ze.getName() = "+ze.getName());
+             OutputStream os = new BufferedOutputStream(new FileOutputStream(getRealFileName(folderPath, ze.getName())));
+             InputStream is = new BufferedInputStream(zfile.getInputStream(ze));
+             int readLen=0;
+             while ((readLen=is.read(buf, 0, 1024))!=-1) {
+                 os.write(buf, 0, readLen);
+             }
+             is.close();
+             os.close();
+         }
+         zfile.close();
+         Log.d("upZipFile", "finishssssssssssssssssssss");
+         return 0;
+     }
+
+
+    public static File getRealFileName(String baseDir, String absFileName) {
+        String[] dirs=absFileName.split("/");
+        String lastDir=baseDir;
+        if(dirs.length>1) {
+            for (int i = 0; i < dirs.length-1;i++) {
+                lastDir +=(dirs[i]+"/");
+                File dir =new File(lastDir);
+                if(!dir.exists()) {
+                    dir.mkdirs();
+                    Log.d("getRealFileName", "create dir = "+(lastDir+"/"+dirs[i]));
+                }
+            }
+            File ret = new File(lastDir,dirs[dirs.length-1]);
+            Log.d("upZipFile", "2ret = "+ret);
+            return ret;
+        } else {
+            return new File(baseDir,absFileName);
+        }
+    }
+}
