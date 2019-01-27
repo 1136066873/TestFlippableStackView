c9c32ab67cb8c2c0598271b25f67f22b0f9e3743
heguodong <heguodong.uh@haier.com>
2019-01-27 16:12:53 +0800

    replace / with file.separator.

--- app/src/main/java/com/haier/demo/testflippablestackview/BannerPathManager.java
+++ app/src/main/java/com/haier/demo/testflippablestackview/BannerPathManager.java
@@ -2,6 +2,8 @@ package com.haier.demo.testflippablestackview;
 
 import android.os.Environment;
 
+import java.io.File;
+
 /**
  * Created by 01438511 on 2019/1/26.
  */
@@ -25,19 +27,19 @@ public class BannerPathManager {
     public String getDeviceRootDirectory(){
         String path ;
         if (isThisDeviceHasSDCard()){
-            path = Environment.getExternalStorageDirectory().getPath() + "/";
+            path = Environment.getExternalStorageDirectory().getPath() + File.separator;
         }else {
-            path = Environment.getRootDirectory().getPath()  + "/";
+            path = Environment.getRootDirectory().getPath()  + File.separator;
         }
         return path;
     }
 
     public String getBannerRootDirectory(){
-        return getDeviceRootDirectory() + Constant.rootFolderNameOfBanner + "/";
+        return getDeviceRootDirectory() + Constant.rootFolderNameOfBanner + File.separator;
     }
 
     public String getBannerDirectory(){
-        return getBannerRootDirectory() + Constant.bannerFolderName + "/" ;
+        return getBannerRootDirectory() + Constant.bannerFolderName + File.separator ;
     }
 
     public String getBannerJsonFilePath(){
@@ -45,11 +47,11 @@ public class BannerPathManager {
     }
 
     public String getBannerImageDirectory(){
-        return getBannerDirectory()  + Constant.bannerImageFolderName + "/" ;
+        return getBannerDirectory()  + Constant.bannerImageFolderName + File.separator ;
     }
 
     public String getBannerAdvertisingResourcesDirectory(){
-        return getBannerDirectory()  + Constant.bannerAdvertisingResourcesFolderName + "/" ;
+        return getBannerDirectory()  + Constant.bannerAdvertisingResourcesFolderName + File.separator ;
     }
 
 }
